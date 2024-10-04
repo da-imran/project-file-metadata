a9c415b5d963815c98d95d3d34f75157c73ef00d
Ehsan <ios.imrannn@gmail.com>
2024-10-04 16:58:27 +0800

    Initial upload

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
 var app = express();
 
 app.use(cors());
@@ -10,9 +13,17 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  try {
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    });
+  } catch (err) {
+    res.send(400);
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
